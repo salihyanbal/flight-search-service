95419d7f859d3b34743df2ada9de77950936c5ee
Salih Yanbal <salihynbl@hotmail.com>
2023-08-24 19:32:28 +0300

    initial commit

---
+++ src/main/java/com/casestudy/flightsearchservice/FlightSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.flightsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchServiceApplication.class, args);
+	}
+
+}
